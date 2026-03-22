48c00b04e7589b1cc4982e9371674bddb140cca8
Timur Ali <alidjati@gmail.com>
2026-03-22 22:27:27 +0300

    Fix package name to ru.aston.hometask

--- ru/aston/hometask/Book.java
+++ ru/aston/hometask/Book.java
@@ -1,4 +1,4 @@
-package Aston.hometask;
+package Aston.ru.aston.hometask;
 
 import java.util.Objects;
 
--- ru/aston/hometask/StudentService.java
+++ ru/aston/hometask/StudentService.java
@@ -1,4 +1,4 @@
-package Aston.hometask;
+package Aston.ru.aston.hometask;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
