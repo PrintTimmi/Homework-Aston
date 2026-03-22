e7dde502d73be1c1c61210430f4181a4401d9fe3
Timur Ali <alidjati@gmail.com>
2026-03-22 20:01:42 +0300

    Add hometask 2

---
+++ hometask/Student.java
@@ -0,0 +1,29 @@
+package Aston.hometask;
+
+import java.util.List;
+
+public class Student {
+
+    private final String name;
+    private final List<Book> books;
+
+    public Student(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student{name='%s', books=%s}", name, books);
+    }
+
+}
+
